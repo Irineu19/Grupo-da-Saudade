70f4d439aa810c7283301c22d51840bc50238cab
Irineu Souza <106166956+Irineu19@users.noreply.github.com>
2025-05-03 22:05:04 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-const imagens = [
-  'imagens/grupodasaudade.jpg',
-  'imagens/grupodasaudade1.jpg',
-  'imagens/grupodasaudade2.jpg',
-  'imagens/gruposdasaudade3.jpg',
-  'imagens/participantes.jpg',
-  'imagens/sertao.jpg',
-  'imagens/apresentacao_sao_joao.jpg',
-  'imagens/carnaval.jpg',
-  'imagens/folia_reis.jpg',
-];
-
-let atual = 0;
-const galeria = document.getElementById('galeria');
-
-setInterval(() => {
-  atual = (atual + 1) % imagens.length;
-  galeria.src = imagens[atual];
-}, 2000); // Troca a imagem a cada 4 segundos
+const fotos = [
+    "imagens/grupodasaudade.jpg",
+    "imagens/grupodasaudade1.jpg",
+    "imagens/grupodasaudade2.jpg",
+    "imagens/apresentacao_sao_joao.jpg",
+    "imagens/carnaval.jpg",
+    "imagens/folia_reis.jpg",
+    "imagens/participantes.jpg",
+    "imagens/sertao.jpg"
+  ];
+  
+  let indice = 0;
+  
+  function mostrarFoto() {
+    document.getElementById("foto").src = fotos[indice];
+  }
+  
+  function anterior() {
+    indice = (indice - 1 + fotos.length) % fotos.length;
+    mostrarFoto();
+  }
+  
+  function proximo() {
+    indice = (indice + 1) % fotos.length;
+    mostrarFoto();
+  }
+  
